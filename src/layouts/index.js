74e82f5afccd77ccf1b93150e3de4a892c49931d
info cube <infocube.dev@gmail.com>
2018-01-25 12:56:25 +0000

    small change

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -28,20 +28,7 @@ class DefaultLayout extends React.Component {
         </Link>
         {this.props.children()}
         <hr style={{ marginTop: rhythm(3) }} />
-        <p>
-          The src for this website is at
-          {` `}
-          <a href="https://github.com/gatsbyjs/gatsby/tree/master/examples/using-contentful">
-            https://github.com/gatsbyjs/gatsby/tree/master/examples/using-contentful
-          </a>
-        </p>
-        <p>
-          The Contentful site that is providing the data for this site is at
-          {` `}
-          <a href="https://discovery.contentful.com/entries/by-content-type/2PqfXUJwE8qSYKuM0U6w8M?delivery_access_token=e481b0f7c5572374474b29f81a91e8ac487bb27d70a6f14dd12142837d8e980a&space_id=ubriaw6jfhm1">
-            https://discovery.contentful.com/entries/by-content-type/2PqfXUJwE8qSYKuM0U6w8M?delivery_access_token=e481b0f7c5572374474b29f81a91e8ac487bb27d70a6f14dd12142837d8e980a&space_id=ubriaw6jfhm1
-          </a>
-        </p>
+        <p>bye</p>
       </div>
     )
   }
