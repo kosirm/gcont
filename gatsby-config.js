442736c5ee6c7fa4c18865378071373d93971c0a
info cube <infocube.dev@gmail.com>
2018-01-25 17:29:21 +0000

    dir name

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,33 @@ module.exports = {
         accessToken: `6f35edf0db39085e9b9c19bd92943e4519c77e72c852d961968665f1324bfc94`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "GatsbyJS",
+        short_name: "GatsbyJS",
+        start_url: "/",
+        background_color: "#f7f0eb",
+        theme_color: "#a2466c",
+        display: "minimal-ui",
+        icons: [
+          {
+            // Everything in /static will be copied to an equivalent
+            // directory in /public during development and build, so
+            // assuming your favicons are in /static/favicons,
+            // you can reference them here
+            src: `/favicons/android-icon-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/android-icon-144x144.png`,
+            sizes: `144x144`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
     `gatsby-transformer-remark`,
     `gatsby-plugin-netlify`,
   ],
